655e3694af357b025f761f5fd0783174c33e7b33
abhi <ExoSupesu@proton.me>
2023-04-23 19:54:46 +0530

    Fixed treesitter  error

--- lua/novo/plugin-manager.lua
+++ lua/novo/plugin-manager.lua
@@ -15,7 +15,7 @@ require("lazy").setup({
     'nvim-telescope/telescope.nvim', tag = '0.1.1',
       dependencies = { 'nvim-lua/plenary.nvim' }
     },
-    {'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'}}
+    {'nvim-treesitter/nvim-treesitter', {build = ':TSUpdate'}}
 })
 require("novo.plugins.Telescope")
 require("novo.plugins.Treesitter")
