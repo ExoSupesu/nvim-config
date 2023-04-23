e0f810886a07a922d7d72fd4d6272dd2fbddd151
abhi <ExoSupesu@proton.me>
2023-04-23 17:48:47 +0530

    Added Treesitter

--- lua/novo/plugin-manager.lua
+++ lua/novo/plugin-manager.lua
@@ -14,6 +14,8 @@ require("lazy").setup({
     {
     'nvim-telescope/telescope.nvim', tag = '0.1.1',
       dependencies = { 'nvim-lua/plenary.nvim' }
-    }
+    },
+    {'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'}}
 })
 require("novo.plugins.Telescope")
+require("novo.plugins.Treesitter")
