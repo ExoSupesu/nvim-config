1dc19fe8327782234d10f40546917dac4bd2219d
abhi <ExoSupesu@proton.me>
2023-04-30 16:43:57 +0530

    New Feature: Transparency

--- lua/novo/colors.lua
+++ lua/novo/colors.lua
@@ -1,5 +1,4 @@
 color = color or "tokyonight-storm"
+vim.g.tokyonight_transparent = vim.g.transparent_enabled
 vim.cmd.colorscheme(color)
-
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+require("transparent").setup()
--- lua/novo/plugin-manager.lua
+++ lua/novo/plugin-manager.lua
@@ -11,6 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
+    {"xiyaowong/transparent.nvim"},
     {
     'nvim-telescope/telescope.nvim', tag = '0.1.1',
       dependencies = { 'nvim-lua/plenary.nvim' }
