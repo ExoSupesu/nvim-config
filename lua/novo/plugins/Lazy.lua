93b419fe3a168fd37e56c0852f16188e012c483a
abhi <ExoSupesu@proton.me>
2023-05-01 19:52:55 +0530

    New Feature: Auto Pairs

--- lua/novo/plugins/Lazy.lua
+++ lua/novo/plugins/Lazy.lua
@@ -11,6 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
+  {"windwp/nvim-autopairs"},
   {"ThePrimeagen/harpoon"},
   {"mbbill/undotree"},
   {
