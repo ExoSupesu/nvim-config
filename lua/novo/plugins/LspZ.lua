8c118b1504c2ded23c37712f6f707fef58441e4f
Abhi <ExoSupesu@proton.me>
2023-05-05 10:27:24 +0530

    feat: 🎸 completetion became better!
    Added config for cmp
    
    BREAKING CHANGE: 🧨 auto complete keystrokes changed!

--- lua/novo/plugins/LspZ.lua
+++ lua/novo/plugins/LspZ.lua
@@ -1,5 +1,47 @@
 local  navic = require("nvim-navic")
-local lsp = require('lsp-zero').preset({
+local lsp = require('lsp-zero')
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+cmp.setup({
+      sources = {
+        {name = 'nvim_lsp'},
+	{ name = "path" },
+        { name = "luasnip" },
+        { name = "cmp_tabnine" },
+        { name = "nvim_lua" },
+        { name = "buffer" },
+        { name = "calc" },
+        { name = "emoji" },
+        { name = "treesitter" },
+        { name = "crates" },
+        { name = "tmux" },
+      },
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+      mapping = {
+	["<C-k>"] = cmp.mapping.select_prev_item(),
+        ["<C-j>"] =  cmp.mapping.select_next_item(),
+        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+	["<Tab>"] = function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif luasnip.expand_or_locally_jumpable() then
+          luasnip.expand_or_jump()
+        elseif jumpable(1) then
+          luasnip.jump(1)
+        elseif has_words_before() then
+          cmp.complete()
+        else
+          fallback()
+        end
+end
+}
+})
+lsp.preset({
   float_border = 'rounded',
   call_servers = 'local',
   configure_diagnostics = true,
