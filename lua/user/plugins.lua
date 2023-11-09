d1af584824090468a0b8005b31c66ddf47a23a07
Abhi <exosupesu@prton.me>
2023-11-09 18:56:54 +0530

    Added auto complete and lsp

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,2 +1,27 @@
-local plugins={}
+local plugins={
+    --ColorSchemes
+    {
+        'AlexvZyl/nordic.nvim',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require 'nordic' .load()
+        end
+    },
+
+    --  LSP
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {'L3MON4D3/LuaSnip'},
+    
+    --Treesitter
+    {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate',
+    },
+}
 return plugins
