e2dff0dc7caefbaeb39b180245cb5d2049adcfc8
Abhi <exosupesu@prton.me>
2023-11-09 19:02:15 +0530

    Added TreeSitter

---
+++ lua/user/treesitter.lua
@@ -0,0 +1,13 @@
+require('nvim-treesitter.configs').setup {
+    ensure_installed = {"lua"}, -- Install parsers for supported languages
+    highlight = {
+    enable = true,              -- Enable syntax highlighting
+    use_languagetree = true,    -- Use the language tree for more accurate highlighting
+  },
+  indent = {
+    enable = true,              -- Enable indenting
+  },
+  autotag = {
+    enable = true,              -- Enable auto-closing and auto-pairing of tags
+  },
+}
