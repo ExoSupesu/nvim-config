4c52adab6d7ac04ffe1108a722b8ef928a8d41a5
Abhi <exosupesu@prton.me>
2023-11-09 18:09:24 +0530

    Added Basic Sturucture And general config

---
+++ lua/user/general.lua
@@ -0,0 +1,72 @@
+
+-- Enable line numbers
+vim.o.number = true
+
+-- Enable relative line numbers
+vim.o.relativenumber = false
+
+-- Enable mouse support
+vim.o.mouse = 'a'
+
+-- Set the background to dark
+vim.o.background = 'dark'
+
+-- Set auto-indentation
+vim.o.autoindent = true
+
+-- Set tabulation settings
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
+-- Highlight matching parentheses
+vim.o.showmatch = true
+
+-- Highlight search results as you type
+vim.o.incsearch = true
+
+-- Enable line wrapping
+vim.o.wrap = true
+
+-- Set the color column at a specific column 
+vim.o.colorcolumn = ''
+
+-- Highlight current line
+vim.o.cursorline = true
+
+-- Display line and column number in the status line
+vim.o.ruler = false
+
+-- Hide the tilde on empty lines
+vim.o.fillchars = 'eob: '
+
+-- Set the default search to be case-insensitive
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Highlight search matches
+vim.o.hlsearch = true
+
+-- Enable line folding
+vim.o.foldenable = true
+
+-- Set the fold level at which folds are closed by default
+vim.o.foldlevelstart = 99
+
+-- Set the encoding of the file
+vim.o.encoding = 'utf-8'
+
+-- Set the line break behavior
+vim.o.linebreak = true
+
+-- Display matching brackets when the cursor is over one
+vim.o.showmatch = true
+
+-- Set the timeout for key mappings
+vim.o.timeoutlen = 500
+
+-- Show line and column number in the command-line
+vim.o.showcmd = true
+
+-- Set highlighting of the current line in normal mode
+vim.o.cursorline = false
